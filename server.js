e815e2b9605c89ea3bbcc63b16ef3d1895cac7d1
showel22 <schowel22@gmail.com>
2018-10-18 17:56:24 -0400

    adding sequalize config

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // For Passport
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized:true}));
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
