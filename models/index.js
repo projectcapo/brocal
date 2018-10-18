2e8963ce9e648f8905b9bd6fd571501e941d2f37
showel22 <schowel22@gmail.com>
2018-10-18 17:47:24 -0400

    adding sequalize config

---
+++ models/index.js
@@ -0,0 +1,34 @@
+let fs = require('fs');
+let path = require('path');
+let Sequelize = require('sequelize');
+let basename = path.basename(module.filename);
+let env = process.env.NODE_ENV || 'development';
+let config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+let db = {};
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
+  })
+  .forEach(function(file) {
+    var model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
