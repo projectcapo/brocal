8c525e9a7ccf2d6026b894d1fd6de98c211ba540
showel22 <schowel22@gmail.com>
2018-10-21 12:06:37 -0400

    Added alerts routes and tied it to the front end with a badge

---
+++ db/alerts-seed.sql
@@ -0,0 +1,5 @@
+use brocal_db;
+INSERT INTO alerts(topic, message, userId, createdAt, updatedAt) VALUES ('Test', "Testing 1", 1, NOW(), NOW());
+INSERT INTO alerts(topic, message, userId, createdAt, updatedAt) VALUES ('Test', "Testing 2", 1, NOW(), NOW());
+INSERT INTO alerts(topic, message, userId, createdAt, updatedAt) VALUES ('Test', "Testing 3", 1, NOW(), NOW());
+INSERT INTO alerts(topic, message, userId, createdAt, updatedAt) VALUES ('Test', "Testing 4", 1, NOW(), NOW());
--- public/assets/js/globals.js
+++ public/assets/js/globals.js
@@ -1,21 +1,24 @@
 $(document).ready(function () {
 
+    function showAlertCount() {
+        $.get("/api/alert/count", function (data) {
+            if (data.count > 0) {
+                $('#alertCount').css('display', "inline");
+                $('#alertCount').text(data.count);
+            }else{
+                $('#alertCount').css('display', "none");
+            }
+        });
+    }
+
     // Get all alerts on an event
     function getAlerts() {
         $.get("/api/alert", function (data) {
             data.forEach(function (alert) {
                 M.toast({ html: alert.topic + ": " + alert.message });
             });
-        })
-    }
-
-    function showAlertCount() {
-        $.get("/api/alert/count", function (data) {
-            if (data.count > 0) {
-                $('#alertCount').css('display', "block");
-                $('#alertCount').text = data.length;
-            }
-        })
+            showAlertCount();
+        });
     }
 
     // Attach get alerts to the alert button on click
