a63f84a8c1995e79061af17b82342523c9248821
Gom Seutail <jose.teixeira9@gee.com>
2018-10-25 17:47:13 -0400

    done

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ require("./routes/graph-routes")(app);
 require("./routes/html-routes.js")(app);
 
 // Commenting out sequalize code for now
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
