d65f0697d8616ec453850bb07909c0af4eaafd78
melton3413 <melton3413@gmail.com>
2018-12-11 20:23:50 -0500

    Added Helmet

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const passport = require('passport');
 const session = require('express-session');
 const env = require('dotenv').load();
+const helmet = require('helmet');
 
 let PORT = process.env.PORT || 8080;
 let app = express();
@@ -11,40 +12,86 @@ let app = express();
 let db = require("./models");
 
 // For Static Content
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // parse application/json
 app.use(bodyParser.json());
 
+/*-------------Helmet.js--------------*/
+
+//Helmet has been initialized in express middleware and will set various HTTP headers to help protect your app.
+app.use(helmet())
+
+//Sets four HTTP headers top prevent loading cached versions of files: Cache-Control, Surrogate-Control, Pragma, and Expires
+app.use(helmet.noCache())
+
+//Helmet’s HSTS set the Strict-Transport-Security header.
+const sixtyDaysInSeconds = 5184000;
+
+//forces browsers to use the https protocol for the website
+app.use(helmet.hsts({
+  maxAge: sixtyDaysInSeconds
+}))
+
+//Hides express server's default x-powered-by header, which can be used by attackers to penetrate the server
+app.disable('x-powered-by');
+
+//prevents clickjacking using frame or iframe
+app.use(helmet.frameguard({
+  action: 'deny'
+}))
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["*"],
+    scriptSrc: ["'self'", 'code.jquery.com/', 'stackpath.bootstrapcdn.com', 'cdnjs.cloudflare.com'],
+    imgSrc: ["*"],
+    fontSrc: ["*"],
+  }
+}))
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); //this line will add a header which will enable CORS
+  next();
+});
+
 // For Passport
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true}));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Set Handlebars.
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 //Load Passport Strategies 
 require('./config/passport/passport.js')(passport, db.user);
 
 // serialize and deserialize user
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user._id);
 });
-passport.deserializeUser(function(id, done) {
-  db.user.findById(id, function(err, user){
-      if(!err){
-         done(null, user);
-      }else{
-         done(err, null);
-      }
-    });
+passport.deserializeUser(function (id, done) {
+  db.user.findById(id, function (err, user) {
+    if (!err) {
+      done(null, user);
+    } else {
+      done(err, null);
+    }
+  });
 });
 
 // ROUTES 
@@ -56,9 +103,11 @@ require("./routes/alert-routes.js")(app);
 require("./routes/graph-routes")(app);
 require("./routes/html-routes.js")(app);
 
-// Commenting out sequalize code for now
-db.sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
+// Commenting out sequelize code for now
+db.sequelize.sync({
+  force: false
+}).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
